7e2a9b39868f95ac66869649d607b8537e5bd7f3
LSM-Shamrock <lsm.shamrock@gmail.com>
2026-05-25 22:07:08 +0900

    FieldObject에 ColliderCenter 속성 추가

--- Assets/01_Scripts/Game/FieldObject.cs
+++ Assets/01_Scripts/Game/FieldObject.cs
@@ -4,4 +4,5 @@ using UnityEngine;
 public abstract class FieldObject : NetworkBehaviour
 {
     public abstract Collider2D Collider { get; }
+    public Vector2 ColliderCenter => Collider.bounds.center;
 }
