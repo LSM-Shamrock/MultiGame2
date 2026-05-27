2e02b295e934425ce128ba09862f1118a753c51d
LSM-Shamrock <167507014+LSM-Shamrock@users.noreply.github.com>
2026-05-27 11:53:43 +0900

    카드데이터를 StaticDB 파일에 넣음

--- Assets/01_Scripts/StaticDB.cs
+++ Assets/01_Scripts/StaticDB.cs
@@ -1,6 +1,40 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+
+public enum LayerType
+{
+    Ground,
+    Air,
+}
+
+public enum TargetingType
+{
+    Core,
+    Ground,
+    GroundOrAir,
+}
+
+public enum ColliderType
+{
+    Normal,
+    Small,
+}
+
+[Serializable]
+public class CardData
+{
+    public int CardId;
+    public string CodeName;
+    public string DisplayName;
+    public int CostMP;
+    public float SummonHeight;
+    public int Health;
+    public LayerType LayerType;
+    public TargetingType TargetingType;
+    public ColliderType ColliderType;
+}
 
 [ExcelAsset]
 public class StaticDB : ScriptableObject
