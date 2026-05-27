4a59ce0715fec8e6a48f2940cb6fbb30450c563a
LSM-Shamrock <167507014+LSM-Shamrock@users.noreply.github.com>
2026-05-27 12:48:48 +0900

    데이터 테이블 수정 등

--- Assets/01_Scripts/StaticDB.cs
+++ Assets/01_Scripts/StaticDB.cs
@@ -45,6 +45,14 @@ public class UnitData
     public ColliderType ColliderType;
 }
 
+[Serializable]
+public class HitData
+{
+    public int HitId;
+    public string CodeName;
+    public int Damage;
+    public float Knockback;
+}
 
 [ExcelAsset]
 public class StaticDB : ScriptableObject
@@ -52,36 +60,19 @@ public class StaticDB : ScriptableObject
     private static StaticDB s_instance;
     public static StaticDB Instance => s_instance ?? (s_instance = Resources.Load<StaticDB>(nameof(StaticDB)));
 
-
-    [SerializeField] private List<CardData> _cards;
-    [SerializeField] private List<UnitData> _units;
-
+    [SerializeField] private List<CardData> _cardDatas;
+    [SerializeField] private List<UnitData> _unitDatas;
+    [SerializeField] private List<HitData> _hitDatas;
 
     private Dictionary<int, CardData> _cardDictionary;
     private Dictionary<int, UnitData> _unitDictionary;
-    public IReadOnlyList<CardData> CardDataList => _cards;    
-    public IReadOnlyList<UnitData> UnitDataList => _units;
+    private Dictionary<int, HitData> _hitDictionary;
 
-    public IReadOnlyDictionary<int, CardData> CardDataTable
-    {
-        get
-        {
-            if (_cardDictionary == null)
-            {
-                _cardDictionary = _cards.ToDictionary(e => e.CardId);
-            }
-            return _cardDictionary;
-        }
-    }
-    public IReadOnlyDictionary<int, UnitData> UnitDataTable
-    {
-        get
-        {
-            if (_unitDictionary == null)
-            {
-                _unitDictionary = _units.ToDictionary(e => e.UnitId);
-            }
-            return _unitDictionary;
-        }
-    }
+    public IReadOnlyList<CardData> CardDataList => _cardDatas;    
+    public IReadOnlyList<UnitData> UnitDataList => _unitDatas;
+    public IReadOnlyList<HitData> HitDataList => _hitDatas;
+
+    public IReadOnlyDictionary<int, CardData> CardDataTable => _cardDictionary ?? (_cardDictionary = _cardDatas.ToDictionary(e => e.CardId));
+    public IReadOnlyDictionary<int, UnitData> UnitDataTable => _unitDictionary ?? (_unitDictionary = _unitDatas.ToDictionary(e => e.UnitId));
+    public IReadOnlyDictionary<int, HitData> HitDataTable => _hitDictionary ?? (_hitDictionary = _hitDatas.ToDictionary(e => e.HitId));
 }
