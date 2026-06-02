1e18f96294fb3d32fd7cc9e1f9e8129fb6c2b1f6
LSM-Shamrock <lsm.shamrock@gmail.com>
2026-06-02 22:10:52 +0900

    게임 종료 ui 제작 중

---
+++ Assets/01_Scripts/Game/GameUI/GameResultUI.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[AutoInjectionTarget]
+public class GameResultUI : MonoBehaviour
+{
+    [SerializeField, ChildField("MainPanel")] private GameObject MainPanel;
+    [SerializeField, ChildField("ResultText")] private TextMeshProUGUI ResultText;
+    [SerializeField, ChildField("SubText")] private TextMeshProUGUI SubText;
+    [SerializeField, ChildField("OkButton")] private Button OkButton;
+}
