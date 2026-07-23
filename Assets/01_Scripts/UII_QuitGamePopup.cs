5b68560df3985efa20cd3b4c3fff4d0508f96494
LSM-Shamrock <lsm.shamrock@gmail.com>
2026-07-24 00:27:21 +0900

    UI_QuitGamePopup 스크립트 제작

---
+++ Assets/01_Scripts/UII_QuitGamePopup.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[AutoInjectionTarget]
+public class UII_QuitGamePopup : MonoBehaviour, IPopupUI
+{
+    Canvas IPopupUI.Canvas => Canvas;
+    GameObject IPopupUI.GameObject => gameObject;
+
+    [SerializeField, ComponentField] private Canvas Canvas;
+    [SerializeField, ChildField] private PointerEventBinder BackPanel;
+    [SerializeField, ChildField] private Button QuitButton;
+    [SerializeField, ChildField] private Button CancleButton;
+
+    private void Start()
+    {
+        BackPanel.AddEvent(PointerEventType.PointerClick, Hide);
+        CancleButton.onClick.AddListener(Hide);
+        QuitButton.onClick.AddListener(OnClickQuitButton);
+    }
+    private void OnDestroy()
+    {
+        BackPanel.RemvoeEvent(PointerEventType.PointerClick, Hide);
+        CancleButton.onClick.RemoveAllListeners();
+        QuitButton.onClick.RemoveAllListeners();
+    }
+
+    private void Hide()
+    {
+        PopupManager.Instance.ClosePopup(this);
+    }
+
+    private void OnClickQuitButton()
+    {
+        Application.Quit();
+    }
+}
