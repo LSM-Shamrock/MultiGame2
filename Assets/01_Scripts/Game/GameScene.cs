795fa83746298952caca71ae25bd4620ee0b2e5c
LSM-Shamrock <lsm.shamrock@gmail.com>
2026-07-24 12:04:58 +0900

    상대가 연결을 종료했을 때, 자신의 승리로 처리하도록 제작

--- Assets/01_Scripts/Game/GameScene.cs
+++ Assets/01_Scripts/Game/GameScene.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 [AutoInjectionTarget]
 public class GameScene : NetworkBehaviour, ISceneInstance<GameScene>
 {
-    [SerializeField, AssetField("Player")]
-    private GameObject _playerPrefab;
+    [SerializeField, AssetField("Player")] private GameObject _playerPrefab;
+    [SerializeField, ChildField("RotationRoot")] private Transform RotationRoot;
 
-    [SerializeField, ChildField("RotationRoot")]
-    private Transform RotationRoot;
+    private PlayerSessionData _localPlayerSessionData;
+    private PlayerSessionData _opponentPlayerSessionData;
 
     public ObservableValue<Player> LocalPlayer { get; private set; } = new();
     public ObservableValue<Player> OpponentPlayer { get; private set; } = new();
@@ -18,19 +18,37 @@ public class GameScene : NetworkBehaviour, ISceneInstance<GameScene>
     public bool IsGameFinished { get; private set; } = false;
     public event Action<ulong?> OnGameFinished;
 
+    private void Awake()
+    {
+        if (NetworkManager.Singleton != null)
+            NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconect;
+    }
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        if (NetworkManager.Singleton != null)
+            NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconect;
+    }
+
     private void Start()
     {
         ((ISceneInstance<GameScene>)this).InitSceneInstance();
 
         if (NetworkManager.Singleton.IsHost && MatchingManager.Instance)
         {
-            var local = MatchingManager.Instance.LocalPlayerSessionData;
-            var localPlayer = SpawnPlayer(local.ClientId, local.PlayerName, local.DeckCardIds, false);
+            _localPlayerSessionData = MatchingManager.Instance.LocalPlayerSessionData;
+            var localPlayer = SpawnPlayer(
+                _localPlayerSessionData.ClientId, 
+                _localPlayerSessionData.PlayerName, 
+                _localPlayerSessionData.DeckCardIds, false);
             
-            var opponent = MatchingManager.Instance.OpponentPlayerSessionData;
-            var opponentPlayer = SpawnPlayer(opponent.ClientId, opponent.PlayerName, opponent.DeckCardIds, true);
-            if (MatchingManager.Instance.MatchingType == MatchingType.PvE)
-                opponentPlayer.IsBot = true;
+            _opponentPlayerSessionData = MatchingManager.Instance.OpponentPlayerSessionData;
+            var opponentPlayer = SpawnPlayer(
+                _opponentPlayerSessionData.ClientId,
+                _opponentPlayerSessionData.PlayerName,
+                _opponentPlayerSessionData.DeckCardIds, true);
+            opponentPlayer.IsBot = MatchingManager.Instance.MatchingType == MatchingType.PvE;
         }
 
         if (IsHost == false)
@@ -63,10 +81,53 @@ public class GameScene : NetworkBehaviour, ISceneInstance<GameScene>
     [Rpc(SendTo.ClientsAndHost)]
     private void FinishGameRpc(bool isDraw, ulong winnerClientId = 0)
     {
+        FinishGameLocal(isDraw, winnerClientId);
+    }
+    private void FinishGameLocal(bool isDraw, ulong winnerClientId = 0)
+    {
         if (IsGameFinished)
             return;
 
         IsGameFinished = true;
         OnGameFinished?.Invoke(isDraw ? null : winnerClientId);
+    }
+
+    private void OnClientDisconect(ulong clientId)
+    {
+        if (!IsGameFinished)
+        {
+            if (IsHost)
+            {
+                if (clientId == _opponentPlayerSessionData.ClientId)
+                {
+                    Debug.Log("상대 클라이언트의 접속이 끊어져 자신의 승리로 처리함.");
+                    FinishGameLocal(false, NetworkManager.LocalClientId);
+                }
+            }
+            else
+            {
+                if (clientId == NetworkManager.LocalClientId)
+                {
+                    bool serverDown = NetworkManager.DisconnectEvent switch
+                    {
+                        NetworkTransport.DisconnectEvents.TransportShutdown         => false,
+                        NetworkTransport.DisconnectEvents.Disconnected              => false,
+                        NetworkTransport.DisconnectEvents.ProtocolTimeout           => true,
+                        NetworkTransport.DisconnectEvents.MaxConnectionAttempts     => false,
+                        NetworkTransport.DisconnectEvents.ClosedByRemote            => true,
+                        NetworkTransport.DisconnectEvents.ClosedRemoteConnection    => false,
+                        NetworkTransport.DisconnectEvents.AuthenticationFailure     => false,
+                        NetworkTransport.DisconnectEvents.ProtocolError             => true,
+                        _ => false
+                    };
+
+                    if (serverDown)
+                    {
+                        Debug.Log("서버의 연결이 끊어져 자신의 승리로 처리함.");
+                        FinishGameLocal(false, NetworkManager.LocalClientId);
+                    }
+                }
+            }
+        }
     }
 }
