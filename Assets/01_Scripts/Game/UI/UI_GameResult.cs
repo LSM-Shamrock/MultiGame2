fe91e02cd4af936ddf7187a44356110fd024dcf3
LSM-Shamrock <lsm.shamrock@gmail.com>
2026-08-04 14:00:19 +0900

    GameResult UI 수정

--- Assets/01_Scripts/Game/UI/UI_GameResult.cs
+++ Assets/01_Scripts/Game/UI/UI_GameResult.cs
@@ -7,13 +7,16 @@ using UnityEngine.UI;
 [AutoInjectionTarget]
 public class UI_GameResult : MonoBehaviour
 {
-    [SerializeField, ChildField("MainPanel")] private Image MainPanel;
-    [SerializeField, ChildField("ResultText")] private TextMeshProUGUI ResultText;
-    [SerializeField, ChildField("SubText")] private TextMeshProUGUI SubText;
-    [SerializeField, ChildField("OkButton")] private Button OkButton;
     [SerializeField, AssetField("Sfx_Game_Result_Win")] private AudioClip _sfx_win;
     [SerializeField, AssetField("Sfx_Game_Result_Lose")] private AudioClip _sfx_lose;
     [SerializeField, AssetField("Sfx_Game_Result_Draw")] private AudioClip _sfx_draw;
+    [SerializeField, ChildField("MainPanel")] private Image MainPanel;
+    [SerializeField, ChildField("OkButton")] private Button OkButton;
+    [SerializeField, ChildField("ResultText")] private TextMeshProUGUI ResultText;
+    [SerializeField, ChildField("LocalPlayerNameText")] private TextMeshProUGUI LocalPlayerNameText;
+    [SerializeField, ChildField("OpponentPlayerNameText")] private TextMeshProUGUI OpponentPlayerNameText;
+    [SerializeField, ChildrenArrayField("LocalCorePointImages")] private Image[] LocalCorePointImages;
+    [SerializeField, ChildrenArrayField("OpponentCorePointImages")] private Image[] OpponentCorePointImages;
 
     private void Start()
     {
@@ -36,43 +39,21 @@ public class UI_GameResult : MonoBehaviour
             ResultText.text = "무승부";
             ResultText.color = Color.gray;
             SoundManager.Instance.PlaySfx(_sfx_draw);
-
-            SubText.text = data.GameFinishType switch
-            {
-                GameFinishType.CoreDestroyed => "양쪽 코어 파괴됨",
-                GameFinishType.Timeout => "시간 초과",
-                _ => ""
-            };
         }
         else if (data.WinnerClientId == NetworkManager.Singleton.LocalClientId)
         {
             ResultText.text = "승리";
             ResultText.color = new Color(0.0f, 0.2f, 1.0f);
             SoundManager.Instance.PlaySfx(_sfx_win);
-
-            SubText.text = data.GameFinishType switch
-            {
-                GameFinishType.CoreDestroyed => "적 코어 파괴 성공",
-                GameFinishType.ClientDisconected => "적 연결 끊김",
-                _ => ""
-            };
         }
         else
         {
             ResultText.text = "패배";
             ResultText.color = new Color(1.0f, 0.0f, 0.0f);
             SoundManager.Instance.PlaySfx(_sfx_lose);
-
-            SubText.text = data.GameFinishType switch
-            {
-                GameFinishType.CoreDestroyed => "내 코어 파괴됨",
-                GameFinishType.ClientDisconected => "내 연결 끊김",
-                _ => ""
-            };
         }
 
         StartCoroutine(ShowRoutine());
-
     }
 
     private IEnumerator ShowRoutine()
